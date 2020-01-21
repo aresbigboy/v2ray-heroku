21c72a2570f41220081b6fdf9e605c4d2e22ef4a
aresbigboy <wei.ares@gmail.com>
2020-01-21 18:30:36 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -7,36 +7,42 @@ rm -rf /usr/bin/v2ray/geosite.dat /usr/bin/v2ray/geoip.dat
 cat <<-EOF > /etc/v2ray/config.json
 {
   "inbounds": [
-   {
-     "protocol": "shadowsocks",
-     "listen":"127.0.0.1",
-     "port": 8081,
-     "settings": {
-      "method": "chacha20-ietf-poly1305",
-      "password": "ssfromheroku20200121",
-      "udp": true
-     },
-      "sniffing": {
-        "enabled": true,
-        "destOverride": ["http", "tls"]
-      }
-   }
+  {
+    "port": ${PORT},
+    "protocol": "vmess",
+    "settings": {
+      "clients": [
+        {
+          "id": "${UUID}",
+          "alterId": 64
+        }
+      ]
+    },
+    "streamSettings": {
+        "network":"ws",
+        "wsSettings":{
+          "path":"/heroku",
+          "headers":{}
+        }
+    }
+  }
   ],
   "outbounds": [
   {
     "protocol": "freedom",
     "settings": {}
   }
-  ]
+  ],
+  "dns": {
+      "servers": [
+          "https+local://1.1.1.1/dns-query",
+          "1.1.1.1",
+          "223.5.5.5",
+          "8.8.8.8",
+          "119.29.29.29",
+          "localhost"
+      ]
+  }
 }
 EOF
-/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json &
-
-git clone https://github.com/shadowsocks/simple-obfs.git
-cd simple-obfs
-git submodule update --init --recursive
-./autogen.sh
-./configure && make
-sudo make install
-
-/usr/local/bin/obfs-server -s 0.0.0.0 -p ${PORT} --obfs http -r 127.0.0.1:8081 --failover huawei.com:80
+/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
